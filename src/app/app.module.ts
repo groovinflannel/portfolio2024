779ff27c2c35b05a975d1e3409ae0e9c56db3760
groovinflannel <groovinflannel@gmail.com>
2023-12-18 14:51:28 -0500

    created experience and portfolio components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EducationComponent
+    EducationComponent,
+    ExperienceComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
